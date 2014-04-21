fe6f83a6d7e9d5994bdf5014caaf9b42f7f9da06
Jacob Roufa <jroufa@fatwallet.com>
2014-04-21 16:54:55 -0400

    tiny-queries! tests passed!

--- src/tiny-jquery.js
+++ src/tiny-jquery.js
@@ -1,2 +1,23 @@
-
 // Your implementation here.
+
+var $$ = tQuery = function( prop, context ) {
+  if ( !( this instanceof tQuery ) )
+  {
+    return new tQuery( prop, context );
+  }
+
+  var elements = ( context !== undefined ) ? context.querySelectorAll( prop ) : document.querySelectorAll( prop );
+
+  for ( var i = 0; i < elements.length; i++ )
+  {
+    this[i] = elements[i];
+  }
+
+  this.length = elements.length;
+
+  this.forEach = function( func ) {
+    return Array.prototype.forEach.call( this, function( elem, index ) {
+      return func.call( elem, elem, index );
+    });
+  };
+};
